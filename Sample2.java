bbf10e4f181d1e584b11f1ab0b5cca83335fbcef
umapatel91 <57888262+umapatel91@users.noreply.github.com>
2019-11-21 16:34:15 +1300

    access modifiers 3

---
+++ Sample2.java
@@ -0,0 +1,19 @@
+package com.accessmodifiers3;
+
+public class Sample2 {
+	
+	// 3. Protected access modifier
+/*	protected int data =10;
+	protected void m1()
+	{
+		System.out.println(data);
+	} */
+	
+	// 4. Public access modifier
+		public int data =10;
+		public void m1()
+		{
+			System.out.println(data);
+		}
+
+}
